6e132e4a00dc91e7993cef5330ba1175ba6fae07
Tate Keller <tatek1993@gmail.com>
2020-02-13 22:06:26 -0600

    Ran initial tests on App.test.js and getByLabelText on ContactForm.spec.js

--- src/components/ContactForm.spec.js
+++ src/components/ContactForm.spec.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { render, getAllByLabelText, fireEvent } from '@testing-library/react';
-import ConactForm from './ContactForm';
+import { render, getByLabelText, getAllByLabelText, fireEvent } from '@testing-library/react';
+import ContactForm from './ContactForm';
+import MutationObserver from "mutationobserver-shim";
 
-TextDecoderStream()
+test('firstName, lastName, email, message are rendered', () => {
+    const { getByLabelText } = render(<ContactForm />);
+    console.log(getByLabelText);
+
+    getByLabelText(/first Name/i);
+    getByLabelText(/last Name/i);
+    getByLabelText(/email/i);
+    getByLabelText(/message/i);
+});
+
+
+
